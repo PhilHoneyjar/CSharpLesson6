78d2aa41e1c5550cd03256280f6dbb5804765bd1
Philipp Medoev <phil.medoev@gmail.com>
2022-12-02 11:57:44 +0300

    Домашка

---
+++ 41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+
+System.Console.Write("Input numbers through spaces: ");
+int[] numbers = System.Array.ConvertAll(System.Console.ReadLine().Split(), int.Parse);
+
+
+System.Console.WriteLine($"Number of array elements greater than 0 = {CountNumbersGreaterZero(numbers)}");
+
+
+int CountNumbersGreaterZero(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+        if (numbers[i] > 0)
+        {
+            count++;
+        }
+    return count;
+}
---
+++ 43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.Write("Input b1: ");
+double b1 = System.Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Input k1: ");
+double k1 = System.Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Input b2: ");
+double b2 = System.Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Input k2: ");
+double k2 = System.Convert.ToInt32(System.Console.ReadLine());
+
+
+if (k1 == k2)
+    System.Console.WriteLine("У заданных прямых нет одной точки пересечения");
+else 
+    System.Console.WriteLine($"Точка пересечения прямых: ({x(b1, k1, b2, k2)};{y(b1, k1, x(b1, k1, b2, k2))})");
+
+
+double x(double b1, double k1, double b2, double k2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+double y(double b, double k, double x)
+{
+    return k * x + b;
+}
